b7d1d907e4ea3689d4fb40d69f6e7824581d4fc4
Srax <thomas_ebsen@live.dk>
2020-08-25 18:23:54 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4,17 +4,14 @@
     if(foundElements != null) {
         for(let i = 0; i < foundElements.length; i++) {
 			if(arrOfNames.indexOf(foundElements[i].innerText.toString()) !== -1){}
-        	else {
-            	arrOfNames.push(foundElements[i].innerText.toString());
-            }
-		}
+        	else {arrOfNames.push(foundElements[i].innerText.toString());}
+        }
     }
     return arrOfNames;
  }
 
 function saveToTxtFile(arr) {
   var textDoc = document.createElement('a');
-
   textDoc.href = 'data:attachment/text,' + encodeURI(arr.join('\n'));
   textDoc.target = '_blank';
   textDoc.download = 'Konkurrence_Deltagere.txt';
