c7af575fe8a96e1092c8c3c1b91dcb48f212fdfc
Srax <thomas_ebsen@live.dk>
2020-08-26 14:25:42 +0200

    virk  nu forhelvede

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
-function saveCommentersNamesToArray() {
+var commentCountElement = document.getElementsByClassName('_3bu3 _293g');
+var loadMoreButton = document.getElementsByClassName(' _4sxc _42ft');
+
+function getListOfNames() {
     let foundElements = document.querySelectorAll('*[class^="_6qw4"]');
     let arrOfNames = [];
     if(foundElements != null) {
@@ -11,14 +14,36 @@ function saveCommentersNamesToArray() {
 }
 
 function saveToTxtFile(arr) {
-  var textDoc = document.createElement('a');
-  textDoc.href = 'data:attachment/text,' + encodeURI(arr.join('\n'));
-  textDoc.target = '_blank';
-  textDoc.download = 'Konkurrence_Deltagere.txt';
-  textDoc.click();
+    try {
+        console.log("Creating File...")
+        var textDoc = document.createElement('a');
+        textDoc.href = 'data:attachment/text,' + encodeURI(arr.join('\n'));
+        textDoc.target = '_blank';
+        textDoc.download = 'Konkurrence_Deltagere.txt';
+        textDoc.click();        
+    } catch (error) {
+        console.log(error);
+    }
  }
 
-var nameArray = saveCommentersNamesToArray();
-saveToTxtFile(nameArray);
 
-console.log("Total Names: " + nameArray.length);
+function loadAllComments() {
+  setTimeout(function() {
+    if (loadMoreButton != null && commentCountElement[0] != null) {
+       console.log("Loading comments...");
+        loadMoreButton[0].click();
+      loadAllComments();
+    } else {
+        console.log("Finished loading comments...");
+        setTimeout(function() {
+            var nameArray = getListOfNames();
+            saveToTxtFile(nameArray);
+            console.log("FINISHED...")
+            console.log("Total Names: " + nameArray.length);
+        }, 250)
+
+    }
+  }, 250)
+}
+
+loadAllComments();
--- app_new.js
+++ app_new.js
@@ -1,5 +1,8 @@
-function saveCommentersNamesToArray() {
-    let foundElements = document.querySelectorAll('*[class^="oi732d6d ik7dh3pa d2edcug0 qv66sw1b c1et5uql a8c37x1j hop8lmos enqfppq2 e9vueds3 j5wam9gi lrazzd5p oo9gr5id"]');
+var commentCountElement = document.getElementsByClassName('_3bu3 _293g');
+var loadMoreButton = document.getElementsByClassName(' _4sxc _42ft');
+
+function getListOfNames() {
+    let foundElements = document.querySelectorAll('*[class^="_6qw4"]');
     let arrOfNames = [];
     if(foundElements != null) {
         for(let i = 0; i < foundElements.length; i++) {
@@ -11,14 +14,36 @@ function saveCommentersNamesToArray() {
 }
 
 function saveToTxtFile(arr) {
-  var textDoc = document.createElement('a');
-  textDoc.href = 'data:attachment/text,' + encodeURI(arr.join('\n'));
-  textDoc.target = '_blank';
-  textDoc.download = 'Konkurrence_Deltagere.txt';
-  textDoc.click();
+    try {
+        console.log("Creating File...")
+        var textDoc = document.createElement('a');
+        textDoc.href = 'data:attachment/text,' + encodeURI(arr.join('\n'));
+        textDoc.target = '_blank';
+        textDoc.download = 'Konkurrence_Deltagere.txt';
+        textDoc.click();        
+    } catch (error) {
+        console.log(error);
+    }
  }
 
-var nameArray = saveCommentersNamesToArray();
-saveToTxtFile(nameArray);
 
-console.log("Total Names: " + nameArray.length);
+function loadAllComments() {
+  setTimeout(function() {
+    if (loadMoreButton != null && commentCountElement[0] != null) {
+       console.log("Loading comments...");
+        loadMoreButton[0].click();
+      loadAllComments();
+    } else {
+        console.log("Finished loading comments...");
+        setTimeout(function() {
+            var nameArray = getListOfNames();
+            saveToTxtFile(nameArray);
+            console.log("FINISHED...")
+            console.log("Total Names: " + nameArray.length);
+        }, 250)
+
+    }
+  }, 250)
+}
+
+loadAllComments();
