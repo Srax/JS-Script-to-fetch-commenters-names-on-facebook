b6d1562c053f9311c6e0fe364e69e44cf2c50336
SraxDBThemes <45574555+SraxDBThemes@users.noreply.github.com>
2020-08-29 16:29:04 +0200

    updated app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var commentCountElement = document.getElementsByClassName('_3bu3 _293g');
-var loadMoreButton = document.getElementsByClassName(' _4sxc _42ft');
+//var loadMoreButton = document.getElementsByClassName(' _4sxc _42ft');
+var loadMoreButtons = document.getElementsByClassName(' _4swz _293g');
 
 function getListOfNames() {
     let foundElements = document.querySelectorAll('*[class^="_6qw4"]');
@@ -29,9 +30,12 @@ function saveToTxtFile(arr) {
 
 function loadAllComments() {
   setTimeout(function() {
-    if (loadMoreButton != null && commentCountElement[0] != null) {
-       console.log("Loading comments...");
-        loadMoreButton[0].click();
+    if (loadMoreButtons != null && commentCountElement[0] != null) {
+        console.log("Loading comments...");
+        for (let i = 0; i < loadMoreButtons.length; i++) {
+            console.log(i);
+            loadMoreButtons[i].firstChild.click();
+        }
       loadAllComments();
     } else {
         console.log("Finished loading comments...");
